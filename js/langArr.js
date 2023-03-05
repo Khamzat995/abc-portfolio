0eae295d5d6897a98ea907be2f0ca91a1c220794
kuduzovkhamzat@gmail.com <khamzat995@gmail.com>
2023-03-05 22:59:35 +0300

    new-lng

---
+++ js/langArr.js
@@ -0,0 +1,218 @@
+const langArr = {
+    "title": {
+        "en": "Khamzat | portfolio ",
+        "tr": "Hamzat | portföy",
+        "ru": "Хамзат | портфолио"
+    },
+    "header-h3": {
+        "en": "Hamzat Kuduzov",
+        "tr": "Hamzat Kuduzov",
+        "ru": "Хамзат Кудузов"
+    },
+    "header-p": {
+        "en": "Full-stack developer",
+        "tr": "Tam yığın geliştirici",
+        "ru": "Full-stack разработчик"
+    },
+    "navbar-home": {
+        "en": "Start",
+        "tr": "Başlangıç",
+        "ru": "Начало"
+    },
+    "navbar-about": {
+        "en": "Experience",
+        "tr": "Deneyim",
+        "ru": "Опыт"
+    },
+    "navbar-services": {
+        "en": "Services",
+        "tr": "Hizmetler",
+        "ru": "Услуги"
+    },
+    "navbar-portfolio": {
+        "en": "Works",
+        "tr": "İşler",
+        "ru": "Работы"
+    },
+    "navbar-contact": {
+        "en": "Contacts",
+        "tr": "Kişiler",
+        "ru": "Контакты"
+    },
+    "content-h3": {
+        "en": "Full stack developer",
+        "tr": "Tam yığın geliştirici",
+        "ru": "Full stack разработчик"
+    },
+    "content-p": {
+        "en": "I am a full cycle developer",
+        "tr": "Ben tam döngü geliştiricisiyim",
+        "ru": "Я разработчик полного цикла программирования"
+    },
+    "content-a": {
+        "en": "View works",
+        "tr": "Çalışmaları görüntüle",
+        "ru": "Посмотреть работы"
+    },
+    "about-span": {
+        "en": "MY",
+        "tr": "BENİM",
+        "ru": "МОЙ"
+    },
+    "heading-h1": {
+        "en": "EXPERIENCE",
+        "tr": "DENEYİMİM",
+        "ru": "ОПЫТ"
+    },
+    "about-p1": {
+        "en": "years as a programmer",
+        "tr": "yıl programcı olarak",
+        "ru": "лет работы программистом"
+    },
+    "about-p2": {
+        "en": "satisfied customers",
+        "tr": "memnun müşteriler",
+        "ru": "довольные клиенты"
+    },
+    "about-p3": {
+        "en": "working hours",
+        "tr": "çalışma saatleri",
+        "ru": "рабочих часов"
+    },
+    "about-p4": {
+        "en": "successful projects",
+        "tr": "başarılı projeler",
+        "ru": "успешных проектов"
+    },
+    "lorem": {
+        "en": "I came into the Fullstack developer profession quite late, but I am very pleased with my new job. A full cycle programmer who can single-handedly create a web application: lay out a page in HTML and CSS, bring it to life with JavaScript, develop the client side in React, and assemble the entire backend with Node. js. This work has opened before me great opportunities for professional and intellectual development.",
+        "tr": "Fullstack geliştirici mesleğine oldukça geç girdim ama yeni işimden çok memnunum. Tek başına bir web uygulaması oluşturabilen tam döngülü bir programcı: HTML ve CSS'de bir sayfa düzenleyin, JavaScript ile hayata geçirin, React'te istemci tarafını geliştirin ve tüm arka ucu Node. js. Bu çalışma, profesyonel ve entelektüel gelişim için önümde büyük fırsatlar açtı.",
+        "ru": "Я пришел в профессию Fullstack-разработчик довольно поздно, однако очень доволен своей новой работой. Программист полного цикла, который в одиночку может создать веб-приложение: сверстать страницу в HTML и CSS, оживить её с помощью JavaScript, разработать клиентскую часть на React и собрать весь бэкенд с помощью Node. js. Эта работа открыла передо мной огромные возможности для профессионального и интеллектуального развития."
+    },
+    "about-btn": {
+        "en": "Contact with me",
+        "tr": "Benimle iletişime geç",
+        "ru": "Связаться со мной"
+    },
+    "progress-h31": {
+        "en": "Web design",
+        "tr": "Web tasarımı",
+        "ru": "Веб дизайн"
+    },
+    "progress-h32": {
+        "en": "Graphic design",
+        "tr": "Grafik tasarım ",
+        "ru": "Графический дизайн"
+    },
+    "services-h1": {
+        "en": "my",
+        "tr": "benim",
+        "ru": "мои"
+    },
+    "services-span": {
+        "en": "services",
+        "tr": "hizmetler ",
+        "ru": "услуги"
+    },
+    "box-h31": {
+        "en": "Website development",
+        "tr": "Web sitesi geliştirme",
+        "ru": "Разработка сайтов"
+    },
+    "box-p1": {
+        "en": "Development of sites of any complexity with an emphasis on the search engine and increasing sales.",
+        "tr": "Arama motoruna ağırlık vererek ve satışları artırarak herhangi bir karmaşıklıktaki sitelerin geliştirilmesi.",
+        "ru": "Разработка сайтов любой сложности с упором на поисковую систему и повышение продаж."
+    },
+    "box-h32": {
+        "en": "Adaptive design",
+        "tr": "Uyarlanabilir tasarım",
+        "ru": "Адаптивный дизайн"
+    },
+    "box-p2": {
+        "en": "A responsive approach to creating a website design that is responsive to the user with the device, screen size and orientation.",
+        "tr": "Cihaz, ekran boyutu ve yönü ile kullanıcıya yanıt veren bir web sitesi tasarımı oluşturmak için duyarlı bir yaklaşım.",
+        "ru": "Отзывчивый подход к созданию дизайна веб-сайта, который реагирует на пользователя с устройством, размер экрана и его ориентацию."
+    },
+    "box-h33": {
+        "en": "Database",
+        "tr": "Veri tabanı",
+        "ru": "Базы данных"
+    },
+    "box-p3": {
+        "en": "Creation, maintenance and use of databases for business and other practical purposes. Database management.",
+        "tr": "İş ve diğer pratik amaçlar için veritabanlarının oluşturulması, bakımı ve kullanımı. Veritabanı Yönetimi.",
+        "ru": "Создание, ведение и использование баз данных для бизнеса и иных практических целей. Управление базами данных."
+    },
+    "box-h34": {
+        "en": "SEO Marketing",
+        "tr": "SEO Pazarlama",
+        "ru": "SEO маркетинг"
+    },
+    "box-p4": {
+        "en": "A set of optimization measures to raise the position of the site in the results of search engines for certain user requests.",
+        "tr": "Belirli kullanıcı istekleri için arama motorlarının sonuçlarında sitenin konumunu yükseltmek için bir dizi optimizasyon önlemi.",
+        "ru": "Комплекс мероприятий по оптимизации для поднятия позиций сайта в результатах выдачи поисковых систем по определённым запросам пользователей."
+    },
+    "portfolio-span": {
+        "en": "my",
+        "tr": "benim",
+        "ru": "мои"
+    },
+    "portfolio-h1": {
+        "en": "work",
+        "tr": "iş",
+        "ru": "работы"
+    },
+    "portfolio-h31": {
+        "en": "Real estate agency",
+        "tr": "Emlakçı",
+        "ru": "Агенство недвижимости"
+    },
+    "portfolio-h32": {
+        "en": "Food to order",
+        "tr": "Sipariş edilecek yemek",
+        "ru": "Еда на заказ"
+    },
+    "portfolio-h33": {
+        "en": "Property in Istanbul",
+        "tr": "İstanbul'da emlak",
+        "ru": "Недвижимость в Стамбуле"
+    },
+    "portfolio-h34": {
+        "en": "Clearing agency",
+        "tr": "Takas ajansı",
+        "ru": "Клиринговое агенство"
+    },
+    "portfolio-h35": {
+        "en": "Clearing agency",
+        "tr": "Takas ajansı",
+        "ru": "Ремонт квартир"
+    },
+    "portfolio-h36": {
+        "en": "Olives and olive oil",
+        "tr": "Zeytin ve zeytinyağı",
+        "ru": "Оливки и оливковое масло"
+    },
+    "portfolio-h37": {
+        "en": "Telegram channel",
+        "tr": "Telegram kanalı",
+        "ru": "Telegram канал"
+    },
+    "portfolio-h38": {
+        "en": "Project under development",
+        "tr": "Geliştirilmekte olan proje",
+        "ru": "Проект в разработке"
+    },
+    "contact-span": {
+        "en": "Contacts",
+        "tr": "Kişiler",
+        "ru": "Контакты"
+    },
+    "footer": {
+        "en": "Istanbul 2021 | All rights reserved",
+        "tr": "Istanbul 2021 | Her hakkı saklıdır",
+        "ru": "Istanbul 2021 | Все права защищены"
+    },
+
+}
---
+++ js/langScript.js
@@ -0,0 +1,30 @@
+const select = document.querySelector('select');
+const allLang = ['en', 'tr', 'ru'];
+
+
+select.addEventListener('change', changeURLLanguage);
+
+// перенаправить на url с указанием языка
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+    for (let key in langArr) {
+        let elements = document.querySelectorAll('.lng-' + key);
+        elements.forEach(elem => {
+            elem.textContent = langArr[key][hash];
+        })
+    }
+}
+
+changeLanguage();
